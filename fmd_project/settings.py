787ff2cdb6815f863b9561dafce6d85a011db695
Atwebembere Boniface <atwebembereboniface@gmail.com>
2025-12-25 02:20:42 +0300

    second

--- fmd_project/settings.py
+++ fmd_project/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'django-insecure-your-secret-key-here'  # Change this in production
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 INSTALLED_APPS = [
